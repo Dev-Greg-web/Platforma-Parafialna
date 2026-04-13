1ecad24f5f7d129be36f1dcb375863b26d6e6edb
Grzegorz Gładysz <grzegorzgladysz2204@gmail.com>
2026-04-13 18:09:23 +0200

    Naprawa tworzenia bazy danych

--- main.py
+++ main.py
@@ -358,7 +358,8 @@ def export_raport():
     nazwa_pliku = f"Raport_Ministranci_{date.today().strftime('%Y-%m-%d')}.xlsx"
     return send_file(output, download_name=nazwa_pliku, as_attachment=True)
 
+with app.app_context(): 
+    db.create_all()
+
 if __name__ == "__main__":
-    with app.app_context(): 
-        db.create_all()
     app.run(debug=True)
